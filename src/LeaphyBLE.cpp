f4e9fbd0d782c7902595bf37c2490529cd296d7b
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-11-30 13:58:49 +0100

    feat: create a binary characteristic

--- src/LeaphyBLE.cpp
+++ src/LeaphyBLE.cpp
@@ -1,5 +1,6 @@
 #include "LeaphyBLE.h"
 #include <ArduinoBLE.h>
+#include <utility/BLEUuid.h>
 
 #define LEAPHY_BLE_SERVICE_UUID "a3a671c0-a063-444d-86b9-34fd0255d897" // This Bluetooth service should be the same for all Leaphy devices. It can be used to separate them from other Bluetooth devices.
 
@@ -15,4 +16,13 @@ bool LeaphyBLE::initialize(const char *deviceName)
   BLE.addService(leaphyService);
   BLE.setAdvertisedService(leaphyService);
   return true;
+}
+
+BLEBoolCharacteristic LeaphyBLE::addBinaryCharacteristic(const char *name, bool initialValue)
+{
+    BLEUuid uuid(name); // Turn name into UUID
+    BLEBoolCharacteristic characteristic(uuid.str(), BLERead | BLEWrite);
+    characteristic.writeValue(initialValue);
+    leaphyService.addCharacteristic(characteristic); // Hook into service
+    return characteristic;
 }
--- src/LeaphyBLE.h
+++ src/LeaphyBLE.h
@@ -1,10 +1,12 @@
 #ifndef _LEAPHY_BLE_H
 #define _LEAPHY_BLE_H
 
+#include <ArduinoBLE.h>
 class LeaphyBLE
 {
 public:
   bool initialize(const char *deviceName);
+  BLEBoolCharacteristic addBinaryCharacteristic(const char *name, bool initialValue);
 };
 
 #endif
