8a32de36f97d3db41f40e2eb6b4e50ab0d1a7250
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-12-14 09:37:08 +0100

    chore: save progress

--- src/LeaphyBLE.h
+++ src/LeaphyBLE.h
@@ -2,11 +2,18 @@
 #define _LEAPHY_BLE_H
 
 #include <ArduinoBLE.h>
+
 class LeaphyBLE
 {
 public:
   bool initialize(const char *deviceName);
   BLEBoolCharacteristic addBinaryCharacteristic(const char *name, bool initialValue);
+  BLEBoolCharacteristic* getCharacteristicByName(const char *name);
+
+private:
+  String characteristicNames[10];
+  BLEBoolCharacteristic* characteristics[10];
+  int characteristicCount = 0;
 };
 
 #endif
